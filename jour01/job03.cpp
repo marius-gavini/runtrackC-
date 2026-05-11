93d44e61da6a164d274849430c309319486667f9
Marius GAVINI <marius.gavini@laplateforme.io>
2026-05-11 13:57:53 +0200

    job03 corriger et job04 et job05 terminés

--- jour01/job03.cpp
+++ jour01/job03.cpp
@@ -3,9 +3,9 @@ int main(void){
     int num;
     std::cout << "Type a number: " << std::endl;
     std::cin >> num;
-    std::cout << "You entered " << num;
+    std::cout << "You entered " << num << std::endl;
     if (num > 0) {
-        for (int i = 0; i < num+1; i++) {
+        for (int i = 0; i < num; i++) {
             std::cout << "Hello World" << std::endl;
         }
     }
---
+++ jour01/job05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+    int a, b;
+
+    std::cout << "Enter the first number : ";
+    std::cin >> a;
+
+    std::cout << "Enter the second number : ";
+    std::cin >> b;
+
+    std::cout << a << " * " << b << " = " << a * b << std::endl;
+}
